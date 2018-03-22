77b961a687249befc7acaf667803138b644f23e9
liusmallpig <chenhuixueba@163.com>
2018-03-22 11:41:32 +0800

    create Project

---
+++ app/src/main/java/com/liuxiaozhu/plugin/MainActivity.java
@@ -0,0 +1,17 @@
+package com.liuxiaozhu.plugin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 测试插件化的DEMO
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
